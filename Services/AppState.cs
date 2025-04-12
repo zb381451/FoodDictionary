8e1a0215b61ecea51b15b362daf4ae11a002fd44
ENambo102 <71136935+ENambo102@users.noreply.github.com>
2025-04-11 21:24:13 -0500

    Add files via upload
    added appstate to help wiht the changing the color theme

---
+++ Services/AppState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDictionary.Services
+{
+    public class AppState
+    {
+        public Color BackgroundColor { get; set; } = Colors.White;
+    }
+}
---
+++ Services/DatabaseService.cs
@@ -0,0 +1,38 @@
+﻿using SQLite;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+public class DatabaseService
+{
+	private static SQLiteAsyncConnection _database;
+	private static readonly string dbName = "FoodDictionarydb.db";
+
+	public async Task InitAsync()
+	{
+		if (_database != null)
+			return;
+
+		string dbPath = Path.Combine(FileSystem.AppDataDirectory, dbName);
+
+		if (!File.Exists(dbPath))
+		{
+			// Update this to your full embedded resource name
+			var resourceName = "FoodDictionary.Services.FoodDictionarydb.db";
+
+			var assembly = Assembly.GetExecutingAssembly();
+			using Stream resourceStream = assembly.GetManifestResourceStream(resourceName)
+				?? throw new FileNotFoundException($"Embedded resource '{resourceName}' not found.");
+
+			using FileStream fileStream = File.Create(dbPath);
+			await resourceStream.CopyToAsync(fileStream);
+		}
+
+		_database = new SQLiteAsyncConnection(dbPath);
+	}
+
+	public static SQLiteAsyncConnection GetConnection()
+	{
+		return _database;
+	}
+}
